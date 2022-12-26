c2054249c719237787efd0273ea084138432317b
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-26 12:00:06 +0530

    ReadOnlyVariable

---
+++ Variable/Six_Variable_ReadOnlyVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿// C# program to show the use
+// of readonly variables
+// without initializing it
+using System;
+
+//Variables declared with the use of ReadOnly Variable ,is called so.
+class Program
+{
+
+    // instance variable
+    int a = 80;
+
+    // static variable
+    static int b = 40;
+
+    // Constant variables
+    const float max = 50;
+
+    // readonly variables
+    readonly int k;
+
+    // Main Method
+    public static void Main()
+    {
+
+        // Creating object
+        Program obj = new Program();
+
+        Console.WriteLine("The value of a is = " + obj.a);
+        Console.WriteLine("The value of b is = " + Program.b);
+        Console.WriteLine("The value of max is = " + Program.max);
+        Console.WriteLine("The value of k is = " + obj.k);
+    }
+}
