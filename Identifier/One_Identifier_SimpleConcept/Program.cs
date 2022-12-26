ad77e1e9dbd73e3d87c71d349254f489e5bda338
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-26 08:27:34 +0530

    Identifier Concepts

---
+++ Identifier/One_Identifier_SimpleConcept/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Name
+{
+    public static void Main(string[] args)
+    {
+        string firstName = "Bhola";
+        string lastName = "Prasad";
+        Console.WriteLine("Hello ,How are you {0} {1},", firstName,lastName);
+
+    }
+}
