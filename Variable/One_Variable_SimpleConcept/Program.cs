8aa394fce1fa8e5dbb35b0ea08736890c8fc147b
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-26 10:59:53 +0530

    local variable

--- Variable/One_Variable_SimpleConcept/Program.cs
+++ Variable/One_Variable_SimpleConcept/Program.cs
@@ -1,4 +1,7 @@
-﻿
+﻿//THERE ARE TOTAL FIVE VARIABLES AVAILABLE :
+//LOCAL VARIABLE //INSTATANCE VARIABLE OR NON-STATIC VARIABLE //STATIC OR CLASS VARIABLE
+//CONSTANT VARIABLE//READONLY VARIABLE 
+
 
  int num = 200;//storing the value to variable
 Console.WriteLine("This is your number {0}", num);//Print the Value stored in Container like Variable
---
+++ Variable/THREE_Variables_LocalVariable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class LocalVariable
+{
+    //variables declared within the code block or method or constructor 
+    // remeber while you are using local variable ,simple method (no use of static )
+    //use static in the main method // create the object of class //call the class with the help of object 
+    public  void DisplayLocalVar()
+    {
+        string userName = "Bhola";
+        int userNameAlphabet = 5;
+        Console.WriteLine("UserName {0} has {1} albhabet in his name:",userName,userNameAlphabet);
+    }
+     public static void Main(string[]Args)
+     {
+        LocalVariable obj= new LocalVariable   ();
+        obj.DisplayLocalVar();
+       
+     }
+
+
+}
