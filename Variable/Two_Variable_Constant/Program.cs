bc95d40220e6d82c8ae5ac893fbfcc8248fed592
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-26 08:47:54 +0530

    Variable (Constant)

--- Variable/Two_Variable_Constant/Program.cs
+++ Variable/Two_Variable_Constant/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int firstNumber = 200;
+
+Console.WriteLine("Number stored in Const is :{0}", firstNumber);
