65e43a16e8d33dd97364df88bd186a069c1e28d3
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-26 08:35:12 +0530

    Variable (Simple)

---
+++ Variable/One_Variable_SimpleConcept/Program.cs
@@ -0,0 +1,5 @@
+﻿
+
+ int num = 200;//storing the value to variable
+Console.WriteLine("This is your number {0}", num);//Print the Value stored in Container like Variable
+Console.ReadLine();// Command to hold the console Window 
